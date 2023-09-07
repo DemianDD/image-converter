4879869704a29deaabc0f215dfd80e55859f01fd
DemianDD <thvarichdemyan@icloud.com>
2023-09-07 15:13:47 +0300

    update output

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,8 +1,14 @@
 const Output = ({ translatedText }: any) => {
   return (
     <div className="w-full">
-      Your Response:
-      <pre>{JSON.stringify(translatedText, null, 2)}</pre>
+      {translatedText != null ? (
+        <div>
+          Your Response:
+          <pre>{JSON.stringify(translatedText, null, 2)}</pre>
+        </div>
+      ) : (
+        <div className="text-center">Waiting for your data...</div>
+      )}
     </div>
   );
 };
